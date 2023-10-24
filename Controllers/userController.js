d920096fea643a6abf1570ee7bc5b9f3fec8f14f
Kipmurkor Emmanuel <emmanuelkipmurkor@gmail.com>
2023-10-24 11:04:12 +0300

    event api

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -6,70 +6,106 @@ dotenv.config();
 
 const userSignup = async (req, res) => {
   try {
-    const { fullName, email, password } = req.body;
-    if (!fullName || !email || !password) {
+    const { fullName, email, password, profession } = req.body;
+    const errors = {};
+
+    if (!fullName) {
+      errors.fullName = "Full Name is required.";
+    }
+    if (!email) {
+      errors.email = "Email is required.";
+    }
+    if (!password) {
+      errors.password = "Password is required.";
+    }
+
+    if (Object.keys(errors).length > 0) {
       return res.status(400).json({
         status: "error",
-        message:
-          "One or more required fields are empty. Please ensure all mandatory fields are filled out.",
+        errors,
       });
     }
-    const user = await authorModel.findOne({ email });
 
-    if (!user) {
-      const hashpaword = await bcrypt.hash(password, 8);
-      const newUser = await authorModel.create({
-        fullName,
-        email,
-        password: hashpaword,
+    const existingUser = await authorModel.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({
+        status: "error",
+        message: "User already registered",
       });
-      await newUser.save();
-      return res
-        .status(200)
-        .json({ status: "succcess", message: "User Successfull Registered" });
-    } else {
-      res
-        .status(409)
-        .json({ status: "error", message: "User already registered " });
     }
+
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    const newUser = await authorModel.create({
+      fullName,
+      email,
+      profession,
+      password: hashedPassword,
+    });
+
+    return res.status(200).json({
+      status: "success",
+      message: "User successfully registered",
+    });
   } catch (error) {
-    return res.status(500).json({ message: error.message });
+    console.error("Error in userSignup:", error);
+    return res.status(500).json({
+      status: "error",
+      message: "An internal server error occurred. Please try again later.",
+    });
   }
 };
 
 const userLogin = async (req, res) => {
   try {
     const { email, password } = req.body;
-    if (!email || !password) {
+    const errors = {};
+
+    if (!email) {
+      errors.email = "Email is required.";
+    }
+    if (!password) {
+      errors.password = "Password is required.";
+    }
+
+    if (Object.keys(errors).length > 0) {
       return res.status(400).json({
         status: "error",
-        message:
-          "Please enter your email and password to log in. All fields are required",
+        errors,
       });
     }
+
     const user = await authorModel.findOne({ email });
     if (user) {
-      const confirmpassword = await bcrypt.compare(password, user.password);
-      if (confirmpassword) {
+      const confirmPassword = await bcrypt.compare(password, user.password);
+      if (confirmPassword) {
         const token = jwt.sign(
           { fullName: user.fullName, email: user.email, author: user._id },
           process.env.JWT_KEY
         );
-        res
-          .status(200)
-          .json({ token, status: "succcess", message: "Successfull Login" });
+        return res.status(200).json({
+          token,
+          status: "success",
+          message: "Successful Login",
+        });
       } else {
         return res.status(401).json({
           status: "error",
           message:
-            "wrong email or password. Please check your credentials and try again.",
+            "Wrong email or password. Please check your credentials and try again.",
         });
       }
     } else {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(404).json({
+        status: "error",
+        message: "User not found",
+      });
     }
   } catch (error) {
-    return res.status(500).json({ error: error.message });
+    return res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
   }
 };
 
